bff3527756372247c6904e52e9523990c83d8ac0
valterEduardoAndrade <eduardovalter255@gmail.com>
2026-06-01 22:14:14 -0300

    feat: integração front-back com media queries e correção de porta do banco

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
-
 const { Pool } = require('pg')
 
 const pool = new Pool({
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT || 5433,
     user: process.env.DB_USER || 'valter_admin',
     password: process.env.DB_PASSWORD || 'bradesco_seguro_2026',
     database: process.env.DB_NAME || 'safecall_bradesco'
